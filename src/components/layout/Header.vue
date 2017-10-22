54d304019fde10d9bcb15f204df82394c3b14bd7
Adnan Unal <a.unal677@gmail.com>
2017-10-22 15:49:46 +0200

    set up vuex and add header

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
+        </a>
+
+        <button class="button navbar-burger is-active">
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import Vuex from 'vuex';
+
+const state = {
+
+};
+
+const store = new Vuex.Store({
+  state,
+});
+
+export default store;
