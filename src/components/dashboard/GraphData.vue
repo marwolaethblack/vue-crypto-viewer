d893f1d497c79d46fb9d5d6b26a3890ce536c0f9
Adnan Unal <a.unal677@gmail.com>
2017-10-29 16:03:25 +0100

    add loading componenet, show loading when loading coin history data from api

--- src/components/dashboard/GraphData.vue
+++ src/components/dashboard/GraphData.vue
@@ -1,20 +1,31 @@
 <template>
   <div>
-    {{ selectedCoin }}
+    <app-loader v-show="isLoading"></app-loader>
+    <p v-show="!isLoading">{{ selectedCoin }}</p>
   </div>
 </template>
 
 <script>
   import { mapActions, mapGetters } from 'vuex';
+  import Loader from '../Loader.vue';
 
   export default {
 
     computed: {
-      ...mapGetters(['coinHistory', 'selectedCoin']),
+      ...mapGetters(['coinHistory', 'selectedCoin', 'loading']),
+      isLoading() {
+        if(this.loading.length != 0) {
+          return this.loading.indexOf('coinHistory') === -1 ? false : true;
+        }
+      }
     },
 
     methods: {
       ...mapActions(['fetchCoinHistory'])
+    },
+
+    components: {
+      'app-loader': Loader
     },
 
     created() {
--- src/store/getters.js
+++ src/store/getters.js
@@ -5,3 +5,5 @@ export const currency = state => state.currency;
 export const selectedCoin = state => state.selectedCoin;
 
 export const coinHistory = state => state.coinHistory;
+
+export const loading = state => state.loading;
